5124cc40926fef8a2e2dc2668cb5b215629731f9
Yisak Metaferiya <yisakmetaferiya1@gmail.com>
2026-01-19 20:37:35 +0400

    feat(user): add user schema with Mongoose integration

---
+++ src/user/schemas/schema.user.ts
@@ -0,0 +1,47 @@
+import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+/**
+ * @Schema()
+ * Turns a TypeScript class into a MongoDB schema that NestJS + Mongoose can use.
+ * @Prop()
+ * Marks a class property as a field in the MongoDB document (like name, email, password, etc.).
+ * --> SchemaFactory.createForClass()
+ * Converts your decorated class into an actual Mongoose schema object.
+ * --> HydratedDocument from mongoose
+ * Represents a fully typed Mongoose document created from your schema.
+ * 
+ */
+
+
+/**
+ * 
+ */
+export type UserDocument = HydratedDocument<User>;
+
+// schema
+@Schema({
+    collection: 'user',
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+        currentTime: () => new Date(),
+    }
+})
+
+export class User{
+    @Prop({required: true})
+    username: string;
+
+    @Prop({required: true})
+    name: string;
+
+    @Prop({required: true})
+    email:string;
+
+    @Prop({required: true})
+    password: string;
+
+}
+
+export const UserSchema = SchemaFactory.createForClass(User)
